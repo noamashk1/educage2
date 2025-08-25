e7bb7b2a8d788ba27845682339e5852a7a7b1ea2
raspberrypi <noam4596@gail.com>
2025-08-25 16:47:31 +0300

    minor changes

--- pythonProject1/restart_experiment.py
+++ pythonProject1/restart_experiment.py
@@ -6,5 +6,8 @@ import os
 current_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, current_dir)
 
-# הפעלת הניסוי מחדש
-os.system(f"python experiment.py --restart try_reatart_25_08_2025")
+print("Restarting experiment automatically...")
+print(f"Experiment name: exp_25_08_2025")
+
+# הפעלת הניסוי מחדש עם הפרמטרים הנכונים
+os.system(f"python experiment.py --restart exp_25_08_2025")
